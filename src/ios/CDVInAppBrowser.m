f9bdc76c7c91b72a1a375b1da6d207b4eeafbf98
rSffsE <marc.belej@coremedicalsolutions.com>
2015-05-21 14:44:47 +0930

    Revert "HTMLMedia Capture crash in iOS8 workaround per https://issues.apache.org/jira/browse/CB-7679"
    This reverts commit e2bea64a9c3950058d4d08af810d11f184fc32c1.

--- src/ios/CDVInAppBrowser.m
+++ src/ios/CDVInAppBrowser.m
@@ -968,13 +968,6 @@
 @implementation CDVInAppBrowserNavigationController : UINavigationController
 
 #pragma mark CDVScreenOrientationDelegate
-- (void)dismissViewControllerAnimated:(BOOL)flag completion:(void (^)(void))completion
-{
-    if ( self.presentedViewController)
-    {
-        [super dismissViewControllerAnimated:flag completion:completion];
-    }
-}
 
 - (BOOL)shouldAutorotate
 {
